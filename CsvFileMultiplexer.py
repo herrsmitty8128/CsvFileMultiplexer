0764832db571a8c89fe270bd5a8ffd144d819cb5
herrsmitty8128 <16559135+herrsmitty8128@users.noreply.github.com>
2022-05-14 07:03:48 -0400

    Updated license name in file header

--- CsvFileMultiplexer.py
+++ CsvFileMultiplexer.py
@@ -1,7 +1,7 @@
 
 #           Copyright Christopher Smith 2022.
 # Distributed under the Boost Software License, Version 1.0.
-#    (See accompanying file LICENSE_1_0.txt or copy at
+#      (See accompanying file LICENSE.txt or copy at
 #          https://www.boost.org/LICENSE_1_0.txt)
 
 from typing import Iterable
